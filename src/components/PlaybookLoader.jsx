e34b8b16da826d33ed7da12b8a0281464d6e2f1d
cdoz88 <corey@fytsolutions.com>
2026-03-26 23:49:59 -0500

    New chalk loader arrows

--- src/components/PlaybookLoader.jsx
+++ src/components/PlaybookLoader.jsx
@@ -12,40 +12,32 @@ export default function PlaybookLoader({ className = "" }) {
         </defs>
         <g filter="url(#chalky-loader)" stroke="white" strokeWidth="2.5" fill="none" strokeLinecap="round" strokeLinejoin="round">
           
-          {/* --- The Xs --- */}
+          {/* --- STATIC FORMATION: The Xs --- */}
           <path d="M 10,15 L 25,35 M 25,15 L 10,35" />
           <path d="M 45,55 L 60,75 M 60,55 L 45,75" />
           <path d="M 120,20 L 135,40 M 135,20 L 120,40" />
 
-          {/* --- The Os --- */}
+          {/* --- STATIC FORMATION: The Os --- */}
           <circle cx="85" cy="25" r="8" />
           <circle cx="30" cy="85" r="8" />
           <circle cx="110" cy="65" r="8" />
           
-          {/* --- The Animated Play Routes (Arrows) --- */}
+          {/* --- THE ANIMATED PLAY ROUTES --- */}
+          {/* Arrowheads are now baked directly into the path string so they draw fluidly! */}
           
-          {/* Route 1: Draws first */}
-          <path d="M 35,25 Q 60,5 75,25" strokeDasharray="60" strokeDashoffset="60">
-            <animate attributeName="stroke-dashoffset" values="60;0" dur="1.2s" begin="0s" fill="freeze" />
-          </path>
-          <path d="M 68,17 L 77,25 L 65,31" opacity="0">
-            <animate attributeName="opacity" values="0;1" dur="0.1s" begin="1.2s" fill="freeze" />
+          {/* Route 1: Draws 0s-1s */}
+          <path d="M 35,25 Q 55,5 75,25 L 65,17 M 75,25 L 65,28" strokeDasharray="72" strokeDashoffset="72">
+            <animate attributeName="stroke-dashoffset" values="72;0;0;72" keyTimes="0;0.25;0.75;1" dur="4s" repeatCount="indefinite" />
           </path>
 
-          {/* Route 2: Draws after Route 1 */}
-          <path d="M 55,65 Q 80,85 100,70" strokeDasharray="60" strokeDashoffset="60">
-            <animate attributeName="stroke-dashoffset" values="60;0" dur="1.2s" begin="1.3s" fill="freeze" />
-          </path>
-          <path d="M 92,62 L 100,70 L 90,78" opacity="0">
-             <animate attributeName="opacity" values="0;1" dur="0.1s" begin="2.5s" fill="freeze" />
+          {/* Route 2: Draws 1s-2s */}
+          <path d="M 55,65 Q 80,85 100,70 L 90,75 M 100,70 L 92,60" strokeDasharray="77" strokeDashoffset="77">
+            <animate attributeName="stroke-dashoffset" values="77;77;0;0;77" keyTimes="0;0.25;0.5;0.75;1" dur="4s" repeatCount="indefinite" />
           </path>
 
-          {/* Route 3: Draws after Route 2 */}
-          <path d="M 120,50 Q 135,70 140,85" strokeDasharray="40" strokeDashoffset="40">
-            <animate attributeName="stroke-dashoffset" values="40;0" dur="1s" begin="2.6s" fill="freeze" />
-          </path>
-          <path d="M 132,78 L 140,85 L 130,88" opacity="0">
-            <animate attributeName="opacity" values="0;1" dur="0.1s" begin="3.6s" fill="freeze" />
+          {/* Route 3: Draws 2s-3s */}
+          <path d="M 120,50 Q 135,70 140,85 L 132,75 M 140,85 L 145,76" strokeDasharray="64" strokeDashoffset="64">
+            <animate attributeName="stroke-dashoffset" values="64;64;0;64" keyTimes="0;0.5;0.75;1" dur="4s" repeatCount="indefinite" />
           </path>
 
         </g>
