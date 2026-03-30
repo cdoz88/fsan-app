173f780bfc2804ec6ea9decc9fa838e9feb17a3b
cdoz88 <corey@fytsolutions.com>
2026-03-29 23:59:59 -0500

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,11 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Fantasy Sports Advice Network",
+  title: "FSAN",
   description: "Win your league with real-time advice.",
+  icons: {
+    icon: "https://fsan.com/wp-content/uploads/2025/05/App-Icons-Border.webp",
+  },
 };
 
 export default function RootLayout({ children }) {
