79b02f5761f811dd74bee259f704ef332a81cb22
cdoz88 <corey@fytsolutions.com>
2026-04-07 09:50:36 -0500

    padding top

--- src/app/football/football-consensus-rankings/page.jsx
+++ src/app/football/football-consensus-rankings/page.jsx
@@ -29,7 +29,8 @@ export default async function ConsensusRankingsPage() {
       <div className="max-w-[1600px] mx-auto px-4 md:px-8 lg:px-10 flex flex-col lg:flex-row gap-8 w-full pb-24">
         <Sidebar activeSport="Football" proToolsMenu={proToolsMenu} connectMenu={connectMenu} />
         
-        <div className="flex-1 w-full pt-6">
+        {/* Removed pt-6 and added min-w-0 to perfectly match the Teams page alignment */}
+        <div className="flex-1 w-full min-w-0">
           <PlayerProvider>
             <ConsensusRanking />
           </PlayerProvider>
