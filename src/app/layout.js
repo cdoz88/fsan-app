45118276788e8864b4f10fb86e5ddedce7e5a43e
cdoz88 <corey@fytsolutions.com>
2026-04-17 22:44:31 -0500

    cookie consent

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import AuthProvider from "../components/AuthProvider";
+import Script from "next/script";
 
 export const metadata = {
   title: "FSAN",
@@ -16,6 +17,12 @@ export default function RootLayout({ children }) {
         <AuthProvider>
           {children}
         </AuthProvider>
+        
+        {/* Global Cookie Consent Banner */}
+        <Script 
+          src="https://gettermscmp.com/cookie-consent/embed/93026241-b406-4893-89b5-fc79b921911f/en-us?auto=true"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
