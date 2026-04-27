78ea052ed3c454768054dedc5f0cfb3a69760806
cdoz88 <corey@fytsolutions.com>
2026-04-26 20:12:12 -0500

    New Analytics code

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,23 @@ export default function RootLayout({ children }) {
           src="https://gettermscmp.com/cookie-consent/embed/93026241-b406-4893-89b5-fc79b921911f/en-us?auto=true"
           strategy="afterInteractive"
         />
+
+        {/* Google Analytics Tracking */}
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-JGTB7799SM" 
+          strategy="afterInteractive" 
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-JGTB7799SM', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
       </body>
     </html>
   );
