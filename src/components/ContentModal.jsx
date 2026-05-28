3d6853c57b2674bfbee86de7f7aad0e83af90ccf
cdoz88 <corey@fytsolutions.com>
2026-05-28 10:24:24 -0500

    Update ContentModal.jsx

--- src/components/ContentModal.jsx
+++ src/components/ContentModal.jsx
@@ -438,7 +438,6 @@ const ShortModalLayout = ({ selectedItem, videos, setSelectedItem, handleShare, 
   );
 };
 
-// UPDATED: Completely replaced Spreaker iframe logic with Acast iframe logic!
 const PodcastModalLayout = ({ selectedItem, handleShare, handleCopy, copied }) => (
   <div className="flex flex-col lg:flex-row h-full min-h-0 overflow-y-auto lg:overflow-hidden">
     <div className="flex-none lg:flex-1 bg-[#0a0a0a] flex flex-col items-center justify-center lg:border-r border-gray-800 p-4 sm:p-6 relative min-h-0">
@@ -454,7 +453,8 @@ const PodcastModalLayout = ({ selectedItem, handleShare, handleCopy, copied }) =
           ></iframe>
         ) : selectedItem.acastShowId ? (
           <iframe 
-            src={`https://embed.acast.com/${selectedItem.acastShowId}`} 
+            // FIX: Added ?feed=true to trigger the playlist view for Master Shows!
+            src={`https://embed.acast.com/${selectedItem.acastShowId}?feed=true`} 
             width="100%" 
             height="100%" 
             frameBorder="0" 
