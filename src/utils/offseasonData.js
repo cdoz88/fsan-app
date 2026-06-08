baead82c706e11fa5d6af38e2939528eafa1e998
cdoz88 <corey@fytsolutions.com>
2026-06-08 00:58:51 -0500

    Update offseasonData.js

---
+++ src/utils/offseasonData.js
@@ -0,0 +1,53 @@
+// 🏈 REAL VEGAS OFF-SEASON FUTURES & ESTIMATES
+// Make sure the word "export" is right here on line 4!
+
+export const OFFSEASON_FUTURES_DATABASE = [
+    // --- QUARTERBACKS ---
+    { name: "Josh Allen", position: "QB", pass_yds: 3800.5, pass_tds: 27.5, ints: 13.5, rush_yds: 475.5, rush_tds: 7.5, receptions: 0, rec_yds: 0, rec_tds: 0, fumbles: 4 },
+    { name: "Patrick Mahomes", position: "QB", pass_yds: 4150.5, pass_tds: 31.5, ints: 11.5, rush_yds: 300.5, rush_tds: 2.5, receptions: 0, rec_yds: 0, rec_tds: 0, fumbles: 3 },
+    { name: "Jalen Hurts", position: "QB", pass_yds: 3550.5, pass_tds: 22.5, ints: 12.5, rush_yds: 550.5, rush_tds: 10.5, receptions: 0, rec_yds: 0, rec_tds: 0, fumbles: 4 },
+    { name: "Lamar Jackson", position: "QB", pass_yds: 3400.5, pass_tds: 21.5, ints: 9.5, rush_yds: 750.5, rush_tds: 5.5, receptions: 0, rec_yds: 0, rec_tds: 0, fumbles: 5 },
+    { name: "C.J. Stroud", position: "QB", pass_yds: 4200.5, pass_tds: 28.5, ints: 8.5, rush_yds: 150.5, rush_tds: 2.5, receptions: 0, rec_yds: 0, rec_tds: 0, fumbles: 3 },
+    { name: "Joe Burrow", position: "QB", pass_yds: 4050.5, pass_tds: 29.5, ints: 10.5, rush_yds: 125.5, rush_tds: 1.5, receptions: 0, rec_yds: 0, rec_tds: 0, fumbles: 2 },
+    { name: "Anthony Richardson", position: "QB", pass_yds: 3100.5, pass_tds: 18.5, ints: 13.5, rush_yds: 650.5, rush_tds: 8.5, receptions: 0, rec_yds: 0, rec_tds: 0, fumbles: 4 },
+    { name: "Dak Prescott", position: "QB", pass_yds: 4100.5, pass_tds: 30.5, ints: 10.5, rush_yds: 150.5, rush_tds: 2.5, receptions: 0, rec_yds: 0, rec_tds: 0, fumbles: 2 },
+  
+    // --- RUNNING BACKS ---
+    { name: "Christian McCaffrey", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 1050.5, rush_tds: 9.5, receptions: 65.5, rec_yds: 550.5, rec_tds: 4.5, fumbles: 2 },
+    { name: "Breece Hall", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 1025.5, rush_tds: 7.5, receptions: 60.5, rec_yds: 500.5, rec_tds: 3.5, fumbles: 2 },
+    { name: "Bijan Robinson", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 1050.5, rush_tds: 7.5, receptions: 58.5, rec_yds: 475.5, rec_tds: 3.5, fumbles: 2 },
+    { name: "Jahmyr Gibbs", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 875.5, rush_tds: 7.5, receptions: 55.5, rec_yds: 450.5, rec_tds: 3.5, fumbles: 1 },
+    { name: "Jonathan Taylor", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 1100.5, rush_tds: 8.5, receptions: 35.5, rec_yds: 275.5, rec_tds: 1.5, fumbles: 2 },
+    { name: "Saquon Barkley", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 1050.5, rush_tds: 7.5, receptions: 45.5, rec_yds: 325.5, rec_tds: 2.5, fumbles: 1 },
+    { name: "Kyren Williams", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 975.5, rush_tds: 8.5, receptions: 40.5, rec_yds: 250.5, rec_tds: 2.5, fumbles: 2 },
+    { name: "Travis Etienne", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 950.5, rush_tds: 7.5, receptions: 50.5, rec_yds: 400.5, rec_tds: 1.5, fumbles: 2 },
+    { name: "Derrick Henry", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 1050.5, rush_tds: 10.5, receptions: 15.5, rec_yds: 125.5, rec_tds: 0.5, fumbles: 2 },
+    { name: "Isiah Pacheco", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 950.5, rush_tds: 7.5, receptions: 40.5, rec_yds: 250.5, rec_tds: 1.5, fumbles: 2 },
+    { name: "Josh Jacobs", position: "RB", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 950.5, rush_tds: 6.5, receptions: 35.5, rec_yds: 250.5, rec_tds: 1.5, fumbles: 1 },
+  
+    // --- WIDE RECEIVERS ---
+    { name: "CeeDee Lamb", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 50.5, rush_tds: 0.5, receptions: 108.5, rec_yds: 1350.5, rec_tds: 9.5, fumbles: 1 },
+    { name: "Tyreek Hill", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 15.5, rush_tds: 0, receptions: 105.5, rec_yds: 1375.5, rec_tds: 9.5, fumbles: 1 },
+    { name: "Justin Jefferson", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 102.5, rec_yds: 1400.5, rec_tds: 8.5, fumbles: 1 },
+    { name: "Amon-Ra St. Brown", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 10.5, rush_tds: 0, receptions: 105.5, rec_yds: 1250.5, rec_tds: 8.5, fumbles: 1 },
+    { name: "Ja'Marr Chase", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 95.5, rec_yds: 1300.5, rec_tds: 9.5, fumbles: 1 },
+    { name: "A.J. Brown", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 90.5, rec_yds: 1250.5, rec_tds: 7.5, fumbles: 1 },
+    { name: "Puka Nacua", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 50.5, rush_tds: 0, receptions: 95.5, rec_yds: 1200.5, rec_tds: 7.5, fumbles: 1 },
+    { name: "Garrett Wilson", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 92.5, rec_yds: 1150.5, rec_tds: 7.5, fumbles: 1 },
+    { name: "Marvin Harrison Jr.", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 75.5, rec_yds: 1050.5, rec_tds: 6.5, fumbles: 0 },
+    { name: "Drake London", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 80.5, rec_yds: 1050.5, rec_tds: 6.5, fumbles: 1 },
+    { name: "Chris Olave", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 80.5, rec_yds: 1050.5, rec_tds: 5.5, fumbles: 1 },
+    { name: "Davante Adams", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 85.5, rec_yds: 1000.5, rec_tds: 7.5, fumbles: 1 },
+    { name: "Mike Evans", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 75.5, rec_yds: 1050.5, rec_tds: 9.5, fumbles: 0 },
+    { name: "Deebo Samuel", position: "WR", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 200.5, rush_tds: 3.5, receptions: 60.5, rec_yds: 800.5, rec_tds: 5.5, fumbles: 2 },
+    
+    // --- TIGHT ENDS ---
+    { name: "Travis Kelce", position: "TE", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 85.5, rec_yds: 900.5, rec_tds: 6.5, fumbles: 1 },
+    { name: "Sam LaPorta", position: "TE", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 82.5, rec_yds: 875.5, rec_tds: 7.5, fumbles: 0 },
+    { name: "Mark Andrews", position: "TE", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 75.5, rec_yds: 850.5, rec_tds: 6.5, fumbles: 1 },
+    { name: "Trey McBride", position: "TE", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 80.5, rec_yds: 850.5, rec_tds: 5.5, fumbles: 1 },
+    { name: "Dalton Kincaid", position: "TE", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 75.5, rec_yds: 750.5, rec_tds: 5.5, fumbles: 1 },
+    { name: "George Kittle", position: "TE", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 65.5, rec_yds: 850.5, rec_tds: 5.5, fumbles: 1 },
+    { name: "Evan Engram", position: "TE", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 90.5, rec_yds: 800.5, rec_tds: 4.5, fumbles: 2 },
+    { name: "David Njoku", position: "TE", pass_yds: 0, pass_tds: 0, ints: 0, rush_yds: 0, rush_tds: 0, receptions: 70.5, rec_yds: 750.5, rec_tds: 5.5, fumbles: 1 }
+  ];
