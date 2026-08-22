d5e3daa4f64a0adfc1021656ae80ee1e51972ab7
cdoz88 <corey@fytsolutions.com>
2026-08-22 12:12:40 -0500

    Remove old files

--- src/app/football/omfg-model/page.jsx
+++ src/app/football/omfg-model/page.jsx
@@ -1,37 +1,5 @@
-import React from 'react';
-import Header from '../../../components/Header';
-import Sidebar from '../../../components/Sidebar';
-import OmfgClient from './OmfgClient';
-import { getMenuBySlug } from '../../../utils/api';
+import { redirect } from 'next/navigation';
 
-export const metadata = {
-  title: 'OMFG Utilization Model | FSAN',
-  description: 'Advanced fantasy football utilization scores and underlying usage metrics.',
-};
-
-export default async function OmfgPage() {
-  let proToolsMenu = [];
-  let connectMenu = [];
-
-  try {
-    if (typeof getMenuBySlug === 'function') {
-      proToolsMenu = await getMenuBySlug('pro-tools-football');
-      connectMenu = await getMenuBySlug('connect-football');
-    }
-  } catch (e) {
-    console.error(e);
-  }
-
-  return (
-    <>
-      <Header activeSport="Football" />
-      <div className="max-w-[1600px] mx-auto px-4 md:px-8 lg:px-10 flex flex-col lg:flex-row gap-8 w-full pb-24">
-        <Sidebar activeSport="Football" proToolsMenu={proToolsMenu} connectMenu={connectMenu} />
-        
-        <div className="flex-1 w-full min-w-0">
-          <OmfgClient />
-        </div>
-      </div>
-    </>
-  );
+export default function OmfgModelRedirect() {
+  redirect('/football/omfg-model/weekly');
 }
